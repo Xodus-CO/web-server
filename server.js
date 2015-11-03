eba9a4962cefdec4393423505fa209c269a847ae
Nathan Mead <nmead33@gmail.com>
2015-11-02 21:51:32 -0700

    Refactor middleware

--- server.js
+++ server.js
@@ -2,16 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function(req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
+
 
 // app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
@@ -20,7 +12,7 @@ app.get('/about', middleware.requireAuthentication, function(req,res) {
 	res.send('About Us!');
 });
 
- app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function(){
 	console.log('Express Server started on port ' + PORT +'!');
